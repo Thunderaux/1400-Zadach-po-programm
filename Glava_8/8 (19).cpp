011f22654aec166abecbae4412623d36131a8700
Thunderaux <perrypm@yandex.com>
2026-01-12 17:12:05 +0300

    Add files via upload

---
+++ Glava_8/8 (19).cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define PI 3.1415926535
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double SquareCirkle, SquareTriengle;
+
+	cout << "Введите площадь круга ";
+	cin >> SquareCirkle;
+
+	cout << "Введите площадь треугольника ";
+	cin >> SquareTriengle;
+
+	bool cirkle = SquareCirkle <= (PI / (3 * sqrt(3))) * SquareTriengle;
+	bool triengle = SquareCirkle >= (4 * PI / (3 * sqrt(3))) * SquareTriengle;
+
+	cout << "а) Круг " << (cirkle ? "уместится" : "не уместится") << " в треугольнике" << endl;
+	cout << "б) Треугольник " << (triengle ? "уместится" : "не уместится") << " в круге" << endl;
+}
---
+++ Glava_8/8 (9).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x, y;
+
+	cout << "Введите x: ";
+	cin >> x;
+
+	cout << "Введите y: ";
+	cin >> y;
+
+	if (x > y)
+		cout << "Максимальное: " << x << endl << "Минимальное: " << y;
+	else
+		cout << "Максимальное: " << y << endl << "Минимальное: " << x;
+
+	return 0;
+}
